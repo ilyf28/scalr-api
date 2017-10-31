b0c991cb4bfbfa3a49186d2e216350fbd1ac1e3b
Suhyun Lee <ilyf28@gmail.com>
2017-10-31 16:16:26 +0900

    save farm role and launch

--- app/Http/Controllers/FarmsController.php
+++ app/Http/Controllers/FarmsController.php
@@ -246,5 +246,159 @@ class FarmsController extends Controller
 
     public function storeFarmRole(Request $request)
     {
+        try {
+            $name = $request->name;
+            $description = $request->description;
+            $launchOrder = $request->launchOrder;
+            $timezone = $request->timezone;
+            $projectId = env('SCALR_PROJECT_ID');
+            $teamsId = env('SCALR_TEAMS_ID');
+            $payload = array(
+                "description" => $description,
+                "launchOrder" => $launchOrder,
+                "name" => $name,
+                "project" => array(
+                    "id" => $projectId
+                ),
+                "teams" => array(
+                    array("id" => $teamsId)
+                ),
+                "timezone" => $timezone
+            );
+
+            $alias = $request->alias;
+            $folder = $request->folder;
+            $computeResource = $request->computeResource;
+            $resourcePool = $request->resourcePool;
+            $dataStore = $request->dataStore;
+            $hosts = $request->hosts;
+            $cloudLocation = 'datacenter-101';
+            $cloudPlatform = 'vmware';
+            $instanceType = $request->instanceType;
+            $roleId = '96542';
+            $network = $request->network;
+
+            $envId = '1';   // hard coding
+            $api_uri = '/api/v1beta0/user/'.$envId.'/farms/';
+            $api_key_id = env('SCALR_API_KEY_ID');
+            $api_secret_key = env('SCALR_API_SECRET_KEY');
+            $body = json_encode($payload);
+            $method = 'POST';
+            $query = '';
+            $date = date('c');
+            $request = array(
+                $method,
+                $date,
+                $api_uri,
+                $query,
+                $body
+            );
+            $signature = 'V1-HMAC-SHA256 ' . base64_encode(hash_hmac('sha256', implode("\n", $request), $api_secret_key, true));
+
+            $response = $this->_client->request($method, env('SCALR_DOMAIN').$api_uri, [
+                'headers' => [
+                    'X-Scalr-Key-Id' => $api_key_id,
+                    'X-Scalr-Date' => $date,
+                    'X-Scalr-Debug' => '1',
+                    'X-Scalr-Signature' => $signature
+                ],
+                'json' => $payload
+            ]);
+
+            if ($response->getStatusCode() === 201) {
+                $result = json_decode($response->getBody());
+
+                $farmId = $result->data->id;
+                $payload = array(
+                    "alias" => $alias,
+                    "cloudFeatures" => array(
+                        "folder" => $folder,
+                        "computeResource" => $computeResource,
+                        "resourcePool" => $resourcePool,
+                        "dataStore" => $dataStore,
+                        "hosts" => array($hosts)
+                    ),
+                    "cloudLocation" => $cloudLocation,
+                    "cloudPlatform" => $cloudPlatform,
+                    "instanceType" => array(
+                        "id" => $instanceType
+                    ),
+                    "role" => array(
+                        "id" => $roleId
+                    ),
+                    "networking" => array(
+                        "networks" => array(
+                            array("id" => $network)
+                        )
+                    )
+                );
+
+                $envId = '1';   // hard coding
+                $api_uri = '/api/v1beta0/user/'.$envId.'/farms/'.$farmId.'/farm-roles/';
+                $api_key_id = env('SCALR_API_KEY_ID');
+                $api_secret_key = env('SCALR_API_SECRET_KEY');
+                $body = json_encode($payload);
+                $method = 'POST';
+                $query = '';
+                $date = date('c');
+                $request = array(
+                    $method,
+                    $date,
+                    $api_uri,
+                    $query,
+                    $body
+                );
+                $signature = 'V1-HMAC-SHA256 ' . base64_encode(hash_hmac('sha256', implode("\n", $request), $api_secret_key, true));
+
+                $response = $this->_client->request($method, env('SCALR_DOMAIN').$api_uri, [
+                    'headers' => [
+                        'X-Scalr-Key-Id' => $api_key_id,
+                        'X-Scalr-Date' => $date,
+                        'X-Scalr-Debug' => '1',
+                        'X-Scalr-Signature' => $signature
+                    ],
+                    'json' => $payload
+                ]);
+
+                if ($response->getStatusCode() === 201) {
+                    $result = json_decode($response->getBody());
+                    
+                    $envId = '1';   // hard coding
+                    $api_uri = '/api/v1beta0/user/'.$envId.'/farms/'.$farmId.'/actions/launch/';
+                    $api_key_id = env('SCALR_API_KEY_ID');
+                    $api_secret_key = env('SCALR_API_SECRET_KEY');
+                    $body = '';
+                    $method = 'POST';
+                    $query = '';
+                    $date = date('c');
+                    $request = array(
+                        $method,
+                        $date,
+                        $api_uri,
+                        $query,
+                        $body
+                    );
+                    $signature = 'V1-HMAC-SHA256 ' . base64_encode(hash_hmac('sha256', implode("\n", $request), $api_secret_key, true));
+
+                    $response = $this->_client->request($method, env('SCALR_DOMAIN').$api_uri, [
+                        'headers' => [
+                            'X-Scalr-Key-Id' => $api_key_id,
+                            'X-Scalr-Date' => $date,
+                            'X-Scalr-Debug' => '1',
+                            'X-Scalr-Signature' => $signature
+                        ]
+                    ]);
+
+                    if ($response->getStatusCode() === 200) {
+                        return redirect('/farms');
+                    } else {
+                    }
+                } else {
+                }
+
+            } else {
+            }
+        } catch (Exception $e) {
+        }
     }
 }
--- resources/views/farm-roles/create.blade.php
+++ resources/views/farm-roles/create.blade.php
@@ -2,7 +2,7 @@
 <h3>Create a new Farm Role in a Farm.</h3>
 <p></p>
 <p>
-    <form method="POST" action="{{ url('/farms') }}">
+    <form method="POST" action="{{ url('/farm-roles') }}">
         <input type="hidden" name="_token" id="csrf-token" value="{{ csrf_token() }}" />
         <p>
             <h4>Farm</h4>
@@ -16,14 +16,14 @@
             <h4>Farm Role</h4>
             <label style="display: inline-block; width: 166px;">Cloud: </label><span>VMware vSphere</span><br />
             <label style="display: inline-block; width: 166px;">Location: </label><span>Service.DC</span><br />
-            <label style="display: inline-block; width: 150px;">Alias: </label><span style="color: red;"> * </span><input type="text" name="alias" /><br />
-            <label style="display: inline-block; width: 150px;">Instance Type: </label><span style="color: red;"> * </span><input type="text" name="instanceType" /><br />
-            <label style="display: inline-block; width: 150px;">Folder: </label><span style="color: red;"> * </span><input type="text" name="folder" /><br />
-            <label style="display: inline-block; width: 150px;">Compute Resource: </label><span style="color: red;"> * </span><input type="text" name="computeResource" /><br />
-            <label style="display: inline-block; width: 150px;">Resource Pool: </label><span style="color: red;"> * </span><input type="text" name="resourcePool" /><br />
-            <label style="display: inline-block; width: 150px;">Host: </label><span style="color: red;"> * </span><input type="text" name="hosts" /><br />
-            <label style="display: inline-block; width: 150px;">Data Store: </label><span style="color: red;"> * </span><input type="text" name="dataStore" /><br />
-            <label style="display: inline-block; width: 150px;">Network: </label><span style="color: red;"> * </span><input type="text" name="network" /><br />
+            <label style="display: inline-block; width: 150px;">Alias: </label><span style="color: red;"> * </span><input type="text" name="alias" value="ubuntu1604-base" /><br />
+            <label style="display: inline-block; width: 150px;">Instance Type: </label><span style="color: red;"> * </span><input type="text" name="instanceType" value="24e3ea31b2d7" /><br />
+            <label style="display: inline-block; width: 150px;">Folder: </label><span style="color: red;"> * </span><input type="text" name="folder" value="group-v108" /><br />
+            <label style="display: inline-block; width: 150px;">Compute Resource: </label><span style="color: red;"> * </span><input type="text" name="computeResource" value="domain-c106" /><br />
+            <label style="display: inline-block; width: 150px;">Resource Pool: </label><span style="color: red;"> * </span><input type="text" name="resourcePool" value="resgroup-107" /><br />
+            <label style="display: inline-block; width: 150px;">Host: </label><span style="color: red;"> * </span><input type="text" name="hosts" value="host-109" /><br />
+            <label style="display: inline-block; width: 150px;">Data Store: </label><span style="color: red;"> * </span><input type="text" name="dataStore" value="datastore-112" /><br />
+            <label style="display: inline-block; width: 150px;">Network: </label><span style="color: red;"> * </span><input type="text" name="network" value="network-111" /><br />
         </p>
         <button type="submit">Farm / Farm Role 생성</button>
     </form>
